7bbfed613d1fd82c2f1f4f3495f5699c1680af15
unknown <pablo.islas@indat.local>
2022-01-09 20:46:20 -0600

    Length check. Return word un uppercase

--- wordle.py
+++ wordle.py
@@ -2,17 +2,14 @@ from spellchecker import SpellChecker
 spell = SpellChecker(language='es')
 
 
-def word_exists(word):
+def word_exists(userWord):
+    word = userWord.lower()
     check = spell.known([word])
-    if word in check:
-        print(word + ' is true')
+    if word in check and len(word) == 5:
+        print(word.upper() + ' is true')
     else:
-        print(word + ' is false')
+        print(word.upper() + ' is false')
 
 
 userWord = input('Ingresa una palabra de 5 letras: ')
 word_exists(userWord)
-
-# we need to pick a random word from pyspellchecker
-# Codecademy final proyect CS.101
-# making tests

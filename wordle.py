dee0f30b82d4c67e352f5826e001cea265c0419f
unknown <pablo.islas@indat.local>
2022-01-09 20:16:27 -0600

    First commit

---
+++ wordle.py
@@ -0,0 +1,17 @@
+from spellchecker import SpellChecker
+spell = SpellChecker(language='es')
+
+
+def word_exists(word):
+    check = spell.known([word])
+    if word in check:
+        print(word + ' is true')
+    else:
+        print(word + ' is false')
+
+
+userWord = input('Ingresa una palabra de 5 letras: ')
+word_exists(userWord)
+
+# we need to pick a random word from pyspellchecker
+# Codecademy final proyect CS.101

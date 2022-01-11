d6116008ec5636e3d13029b677e9d5bed6e6c323
unknown <pablo.islas@indat.local>
2022-01-11 12:28:08 -0600

    Add strings of Letters in word and Letters not in word

--- wordle.py
+++ wordle.py
@@ -8,20 +8,43 @@ def choose_word():
     while len(random_word) != 5:
         random_word = random.choice([word for word in spell])
         if len(random_word) == 5:
-            game_word = random_word
-            print(game_word)
+            print(random_word)
+            return random_word
 
 
-choose_word()
+game_word = choose_word()
 
 
 def word_exists(userWord):
     word = userWord.lower()
     check = spell.known([word])
     if word in check and len(word) == 5:
-        print(word.upper() + ' is true')
+        print(word.upper() + ' is playing')
+        play(word, game_word)
     else:
         print(word.upper() + ' is not a valid word')
+
+
+def play(word, game_word):
+    if word == game_word:
+        print('You won!')
+    else:
+        result_word = ''
+        letters_in_word = ''
+        letters_not_in_word = ''
+        for i in range(len(word)):
+            if word[i] == game_word[i]:
+                result_word = result_word + game_word[i].upper()
+            elif word[i] in game_word:
+                result_word += "_"
+                letters_in_word += word[i]
+            elif word[i] not in game_word:
+                result_word += "_"
+                letters_not_in_word += word[i]
+        print(result_word)
+        print('Letters in word: ' + letters_in_word)
+        print('Letters not in word: ' + letters_not_in_word)
+        print(' ')
 
 
 userWord = input('Ingresa una palabra de 5 letras: ')
